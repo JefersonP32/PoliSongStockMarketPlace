92822b560b1b4db3baa1875170c5dd2102cd6eef
Jeferson <Jeferson@DESKTOP-BRIDG0H.bbrouter>
2025-11-12 22:40:02 -0500

    Se agregó y estilizó la tabla de recopilaciones del comprador

--- src/main/java/com/polisong/view/Recopilaciones.java
+++ src/main/java/com/polisong/view/Recopilaciones.java
@@ -1,6 +1,6 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JPanel.java to edit this template
+ * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
 package com.polisong.view;
 
@@ -8,13 +8,14 @@ package com.polisong.view;
  *
  * @author Jeferson
  */
-public class Recopilaciones extends javax.swing.JPanel {
+public class Recopilaciones extends javax.swing.JFrame {
 
     /**
-     * Creates new form FrmInicio
+     * Creates new form Recopilaciones
      */
     public Recopilaciones() {
         initComponents();
+        personalizarTabla();
     }
 
     /**
@@ -28,63 +29,140 @@ public class Recopilaciones extends javax.swing.JPanel {
 
         jScrollPane1 = new javax.swing.JScrollPane();
         jTable1 = new javax.swing.JTable();
-        jLabel1 = new javax.swing.JLabel();
         jButton1 = new javax.swing.JButton();
         jButton2 = new javax.swing.JButton();
+        jLabel1 = new javax.swing.JLabel();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setMaximizedBounds(new java.awt.Rectangle(0, 0, 1200, 600));
+        setMinimumSize(new java.awt.Dimension(1200, 660));
+        setSize(new java.awt.Dimension(1200, 660));
+        getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
         jTable1.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null}
+                {null, null, null},
+                {null, null, null},
+                {null, null, null},
+                {null, null, null}
             },
             new String [] {
-                "Title 1", "Title 2", "Title 3", "Title 4"
+                "ID", "Nombre", "Descripción"
             }
         ));
         jScrollPane1.setViewportView(jTable1);
+        if (jTable1.getColumnModel().getColumnCount() > 0) {
+            jTable1.getColumnModel().getColumn(0).setPreferredWidth(1);
+        }
 
-        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/Recopilaciones.png"))); // NOI18N
-        jLabel1.setText("jLabel1");
+        getContentPane().add(jScrollPane1, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 260, 460, 210));
 
-        jButton1.setText("Agregar");
+        jButton1.setText("Eliminar");
         jButton1.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jButton1ActionPerformed(evt);
             }
         });
+        getContentPane().add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 220, -1, -1));
 
-        jButton2.setText("Eliminar");
+        jButton2.setText("Crear Playlist");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
+        getContentPane().add(jButton2, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 170, -1, -1));
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
-        this.setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addComponent(jButton1)
-                    .addGroup(javax.swing.GroupLayout.Alignment.LEADING, layout.createSequentialGroup()
-                        .addContainerGap()
-                        .addComponent(jButton2)))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 1403, javax.swing.GroupLayout.PREFERRED_SIZE))
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-            .addGroup(layout.createSequentialGroup()
-                .addGap(210, 210, 210)
-                .addComponent(jButton1)
-                .addGap(31, 31, 31)
-                .addComponent(jButton2)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-        );
+        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/Recopilaciones.png"))); // NOI18N
+        jLabel1.setText("jLabel1");
+        getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(-160, -30, 1360, 680));
+
+        pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton2ActionPerformed
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         // TODO add your handling code here:
     }//GEN-LAST:event_jButton1ActionPerformed
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(Recopilaciones.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(Recopilaciones.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(Recopilaciones.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(Recopilaciones.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new Recopilaciones().setVisible(true);
+            }
+        });
+    }
+    
+    private void personalizarTabla() {
+    // 🔹 Fuente base moderna
+    jTable1.setFont(new java.awt.Font("Segoe UI", java.awt.Font.PLAIN, 14));
+    jTable1.setRowHeight(30);
+    jTable1.setBackground(java.awt.Color.WHITE);
+    jTable1.setForeground(new java.awt.Color(40, 40, 40));
+
+    // 🎨 Renderer personalizado para forzar color del encabezado
+    javax.swing.table.JTableHeader header = jTable1.getTableHeader();
+    header.setDefaultRenderer((table, value, isSelected, hasFocus, row, column) -> {
+        javax.swing.JLabel lbl = new javax.swing.JLabel(value.toString());
+        lbl.setOpaque(true);
+        lbl.setBackground(new java.awt.Color(96, 96, 96));   // gris oscuro #606060
+        lbl.setForeground(new java.awt.Color(240, 240, 240)); // texto claro
+        lbl.setFont(new java.awt.Font("Segoe UI Semibold", java.awt.Font.PLAIN, 13));
+        lbl.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        lbl.setBorder(javax.swing.BorderFactory.createMatteBorder(0, 0, 1, 0, new java.awt.Color(80, 80, 80))); // línea inferior sutil
+        return lbl;
+    });
+
+    // 🔹 Quitar líneas verticales y dejar solo horizontales suaves
+    jTable1.setShowHorizontalLines(true);
+    jTable1.setShowVerticalLines(false);
+    jTable1.setGridColor(new java.awt.Color(230, 230, 230));
+    jTable1.setIntercellSpacing(new java.awt.Dimension(0, 1));
+
+    // 🔹 Padding interno tipo web
+    javax.swing.table.DefaultTableCellRenderer cellRenderer = new javax.swing.table.DefaultTableCellRenderer();
+    cellRenderer.setBorder(javax.swing.BorderFactory.createEmptyBorder(8, 14, 8, 14));
+    jTable1.setDefaultRenderer(Object.class, cellRenderer);
+
+    // 🔹 Selección moderna
+    jTable1.setSelectionBackground(new java.awt.Color(230, 240, 255));
+    jTable1.setSelectionForeground(java.awt.Color.BLACK);
+
+    // 🔹 Quitar bordes del scroll
+    jScrollPane1.setBorder(javax.swing.BorderFactory.createEmptyBorder());
+    jScrollPane1.getViewport().setBackground(java.awt.Color.WHITE);
+}
+
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
